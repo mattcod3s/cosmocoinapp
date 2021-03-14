2d9990345c696b997cb90c9c6a94fd648564b45a
MattCod3s <matthew.kostka08@gmail.com>
2021-03-14 00:49:52 +0000

    backend set up

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,11 +1,42 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors'
+let express = require('express');
+let cors  = require('cors');
+let mongoose = require('mongoose');
+let database = require('./database');
+let bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
+mongoose.connect(database.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database Connected');
+ }, 
+ error => {
+     console.log('Database Couldnt be connected to: ' + error);
+ }
+)
+
+const cryptoEndPoint = require('./routes/crypto.route');
 const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cors());
 
+app.use('/api', cryptoEndPoint);
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log('Connected to port: ' + port);
+})
+
+app.use((req,res,next) => {
+    next(createError(404));
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if(!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+})
---
+++ root/backend/models/Crypto.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let cryptoSchema = new Schema({
+    name: {
+        type: String
+    }
+}, {
+    collection: 'cryptos'
+})
+
+module.exports = mongoose.model('Crypto', cryptoSchema);
