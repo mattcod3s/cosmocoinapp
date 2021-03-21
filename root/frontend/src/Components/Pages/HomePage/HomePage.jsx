b8d28c452d48380ecdc78e568663af0c0d28a650
MattCod3s <matthew.kostka08@gmail.com>
2021-03-21 01:53:35 +0000

    login page started

--- root/frontend/src/Components/Pages/HomePage/HomePage.jsx
+++ root/frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './homePageStyles.scss';
-import HomeBackground from './HomeBackground/HomeBackground';
+import { LoginPage } from "../LoginPage/LoginPage";
+import {Route, BrowserRouter as Router, Switch, useLocation, Link} from 'react-router-dom';
 
 const HomePage = () => {
+    const loc = useLocation();
     return (
         <div className="home__page">
-            <HomeBackground />
             <div className="home__card">
                 <div className="logo"></div>
                 <div className="home__blurb">
                     <h3>Number One Platform to keep track of your favourite Cryptocurrencies!</h3>
                 </div>
                 <div className="home__btns">
-                    <div className="home__btn btn__1">Sign In</div>
-                    <div className="home__btn btn__1">Register</div>
+                    <Link style={{ textDecoration: 'none' }} to="/login" className="home__btn btn__1"><h3>Sign In</h3></Link>
+                    <Link style={{ textDecoration: 'none' }} to="/login" className="home__btn btn__1"><h3>Register</h3></Link>
                 </div>
             </div>
         </div>
--- root/frontend/src/Components/Pages/LoginPage/LoginPage.jsx
+++ root/frontend/src/Components/Pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './loginPageStyles.scss';
 
 const LoginPage = () => {
     return (
-        <div>
-            
+        <div className="login__page">
+            <div className="login__container"></div>
         </div>
     )
 }
